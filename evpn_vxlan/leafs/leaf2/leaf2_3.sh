c32c845f3a9d72688bc0fec1e12dace19cad2c48
piercirocaliandro <piercirocaliandro998@gmail.com>
2022-08-01 14:56:53 +0200

    #1: evpn/vxlan configuration for datacenter

---
+++ evpn_vxlan/leafs/leaf2/leaf2_3.sh
@@ -0,0 +1,18 @@
+# Default routes for the end-hosts
+net add vlan 10 ip address 10.1.1.254/24
+net add vlan 20 ip address 10.0.0.254/24
+
+# VXLAN interface for L3VNI
+net add vlan 50
+net add vxlan vni-1020 vxlan id 1020
+net add vxlan vni-1020 vxlan local-tunnelip 2.2.2.2
+net add vxlan vni-1020 bridge access 50
+
+# Tenant VRF
+net add vrf TEN1 vni 1020
+net add vlan 50 vrf TEN1
+net add vlan 10 vrf TEN1
+net add vlan 20 vrf TEN1
+
+net pending
+net commit
